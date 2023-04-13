8163b03b2e784f256c7f713a4acaec8cd6acc80c
SandraKoch <sandraannakoch@gmail.com>
2023-04-13 18:59:29 +0200

    task2

--- HW_6/js/task-02.js
+++ HW_6/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIngredientsElement = document.querySelector("#ingredients");
+console.log(listOfIngredientsElement);
+
+ingredients.forEach((element) => {
+  const listItem = document.createElement("li");
+
+  listItem.classList.add("item");
+
+  listItem.innerHTML = element;
+  listOfIngredientsElement.appendChild(listItem);
+});

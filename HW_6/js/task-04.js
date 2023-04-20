7e29fb0611d433ea62e197278367c671c162240d
SandraKoch <sandraannakoch@gmail.com>
2023-04-20 21:28:51 +0200

    task 4 update

--- HW_6/js/task-04.js
+++ HW_6/js/task-04.js
@@ -1,15 +1,23 @@
-const counterValue = document.querySelector("#value");
-console.log(counterValue);
+// const counterValue = document.querySelector("#value");
+let counterValue = 0;
+// console.log(counterValue);
+
+const counterEl = document.querySelector("#value");
 
 const incrementButton = document.querySelector('[data-action="increment"]');
 const decrementButton = document.querySelector('[data-action="decrement"]');
 
 incrementButton.addEventListener("click", () => {
-  let incrementedValue = +counterValue.innerHTML; //+ changes string to a number
-  counterValue.innerHTML = incrementedValue += 1;
+  let incrementedValue = (counterValue += 1);
+  counterEl.innerHTML = incrementedValue;
+
+  // let incrementedValue = +counterValue.innerHTML; //+ changes string to a number
+  // counterValue.innerHTML = incrementedValue += 1;
 });
 
 decrementButton.addEventListener("click", () => {
-  let decrementedValue = Number.parseInt(counterValue.innerHTML);
-  counterValue.innerHTML = decrementedValue -= 1;
+  let decrementedValue = (counterValue -= 1);
+  counterEl.innerHTML = decrementedValue;
+  // let decrementedValue = Number.parseInt(counterValue.innerHTML);
+  // counterValue.innerHTML = decrementedValue -= 1;
 });
